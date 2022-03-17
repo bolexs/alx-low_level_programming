198b408d96a77fcec2e09e2ccc8736ee3eabf510
bolexs <boluwatufeadeojo@gmail.com>
2022-03-17 03:03:58 -0700

    time

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,44 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the n times table
+ * print_times_table - prints the n times table, starting with 0
  *
- * @n: number times table (0 < n <= 15)
- *
- * Return: no return
+ * @n: int param
  */
+
 void print_times_table(int n)
 {
-	int a, b, op;
+	int num, mul, prod;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <= n; a++)
+		for (num = 0; num <= n; num++)
 		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
+
+			_putchar('0');
+			for (mul = 1; mul <= n; mul++)
 			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
+				_putchar(',');
+				_putchar(' ');
+				prod = num * mul;
+				if (prod <= 99)
 				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
+					_putchar(' ');
 				}
-				else if (op <= 99)
+				if (prod <= 9)
 				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar(' ');
 				}
-				else
+				if (prod >= 100)
 				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar((prod / 100) + '0');
+					_putchar((prod / 10) % 10 + '0');
 				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
