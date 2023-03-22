c76379c32b7e6497ddaca508e16a0d6438b9e836
bolexs <boluwatufeadeojo@gmail.com>
2023-03-22 13:53:47 -0700

    changes and modifications

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,13 +8,9 @@
 
 void print_alphabet(void)
 {
-char alphabet;
+	char a = 'a';
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
-}
-
-_putchar('\n');
-
+	for (; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
 }
