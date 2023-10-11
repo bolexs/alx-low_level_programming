4006856bcc5f7d9706ee2520ff58b01146a73242
bolexs <boluwatufeadeojo@gmail.com>
2023-10-11 05:17:14 -0700

    first commit

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,26 +13,25 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, right_t, left_t;
+	size_t i, right, left;
 
 	if (array == NULL)
 		return (-1);
 
-	for (left_t = 0, right_t = size - 1; right_t >= left_t;)
+	for (left = 0, right = size - 1; right >= left;)
 	{
 		printf("Searching in array: ");
-		for (i = left_t; i < right_t; i++)
+		for (i = left; i < right; i++)
 			printf("%d, ", array[i]);
 		printf("%d\n", array[i]);
 
-		i = left_t + (right_t - left_t) / 2;
+		i = left + (right - left) / 2;
 		if (array[i] == value)
 			return (i);
 		if (array[i] > value)
-			right_t = i - 1;
+			right = i - 1;
 		else
-			left_t = i + 1;
+			left = i + 1;
 	}
 	return (-1);
 }
-
