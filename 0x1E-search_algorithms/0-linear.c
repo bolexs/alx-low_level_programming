fefe0c0020ffc2ed58ecaaf820e1499d8f3e1199
bolexs <boluwatufeadeojo@gmail.com>
2023-10-11 05:14:27 -0700

    first commit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (indx = 0; indx < size; indx++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", idx, array[indx]);
+		printf("Value checked array[%lu] = [%d]\n", indx, array[indx]);
 
 		if (array[indx] == value)
 		{
